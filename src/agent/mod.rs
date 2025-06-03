7dc109ebbc87f82591c5058da07e32bd71451ab2
wesuRage <weslley.nmiranda@gmail.com>
2025-06-02 22:28:10 -0300

    feat(deps): added core dependencies

--- src/agent/mod.rs
+++
@@ -1,5 +0,0 @@
-pub mod exec;
-pub mod id_management;
-pub mod poll_command;
-pub mod registration;
-pub mod server_health;
--- src/operator/mod.rs
+++
@@ -1,4 +0,0 @@
-pub mod list_agents;
-pub mod send_command;
-pub mod interactive_shell;
-pub mod broadcast_command;
